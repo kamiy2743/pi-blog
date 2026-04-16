cd7b24e6e45c09a3cd190c0bbbebf43acd562f05
kamiy2743 <kamiy2743@gmail.com>
2026-04-16 18:22:21 +0900

    直接ステータス指定

--- backend/internal/handler/feature/top/show/show_top_test.go
+++ backend/internal/handler/feature/top/show/show_top_test.go
@@ -95,7 +95,7 @@ func Test記事の取得に失敗した場合は500(t *testing.T) {
 	setUpRecords(t, initResult.EntClient)
 
 	res := callEndpoint(t, initResult.Server)
-	res.AssertError(t, http.StatusInternalServerError, "記事の読み込みに失敗しました。", "時間をおいてから、もう一度お試しください。")
+	res.AssertError(t, 500, "記事の読み込みに失敗しました。", "時間をおいてから、もう一度お試しください。")
 }
 
 func Testカテゴリの取得に失敗した場合は500(t *testing.T) {
@@ -109,7 +109,7 @@ func Testカテゴリの取得に失敗した場合は500(t *testing.T) {
 	setUpRecords(t, initResult.EntClient)
 
 	res := callEndpoint(t, initResult.Server)
-	res.AssertError(t, http.StatusInternalServerError, "カテゴリの読み込みに失敗しました。", "時間をおいてから、もう一度お試しください。")
+	res.AssertError(t, 500, "カテゴリの読み込みに失敗しました。", "時間をおいてから、もう一度お試しください。")
 }
 
 func setUpRecords(t *testing.T, entClient *ent.Client) records {
